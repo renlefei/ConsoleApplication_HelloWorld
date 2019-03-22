d432a284a10e0f2d05c31d5d877cb2cb1acac48a
renlefei <renlefei16@163.com>
2019-03-23 03:54:12 +0800

    My first commit

---
+++ ConsoleApplication_HelloWorld/ConsoleApplication_HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication_HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
